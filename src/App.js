5e188696e01c9dc21a99e4cf650aedb0b4e1036a
ThanhTra Le <thanhtra.le@ogilvy.com>
2018-08-30 17:25:05 +0700

    restructure css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import "./assets/css/app.scss"
-import "font-awesome/css/font-awesome.css";
 import Header from "./components/header/header"
 import SearchMain from "./components/SearchMain/searchMain"
 class App extends Component {
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import logo from "./../../assets/images/logo.svg"
+import logo from "./../../logo.svg"
 import './header.scss'
 
 export default class Header extends Component {
